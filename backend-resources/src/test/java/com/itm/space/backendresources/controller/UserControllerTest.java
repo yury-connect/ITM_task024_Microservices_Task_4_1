652a28e33add633ee1f903d2bec761e77eccd68d
Yury_Home-PC_Win10_worked <yury.connect@gmail.com>
2024-11-16 11:04:02 +0300

    save

---
+++ backend-resources/src/test/java/com/itm/space/backendresources/controller/UserControllerTest.java
@@ -0,0 +1,22 @@
+package com.itm.space.backendresources.controller;
+
+import com.itm.space.backendresources.BaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class UserControllerTest extends BaseIntegrationTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void hello() {
+    }
+}
